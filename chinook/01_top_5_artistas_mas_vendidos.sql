0f4951f2f980b6b2d36916af566ad7d16453b085
nicolas097 <nicolasolivares097@gmail.com>
2025-05-01 00:12:26 -0400

    Ejercicio n°1 Terminado

--- chinook/01_top_5_artistas_mas_vendidos.sql
+++ chinook/01_top_5_artistas_mas_vendidos.sql
@@ -1,5 +1,10 @@
-CREATE TABLE empleados (
-  id INT PRIMARY KEY,
-  nombre VARCHAR(100),
-  salario DECIMAL(10,2)
-);
+	SELECT 
+			 COUNT(*) AS cantidad_vendida,
+			 ar.`Name` AS artista
+	FROM invoiceline il
+	INNER JOIN track tr ON tr.TrackId = il.TrackId
+	INNER JOIN album al ON al.AlbumId = tr.AlbumId
+	INNER JOIN artist ar ON ar.ArtistId = al.ArtistId
+	GROUP BY ar.`Name`
+	ORDER BY 1 DESC
+	LIMIT 5;
