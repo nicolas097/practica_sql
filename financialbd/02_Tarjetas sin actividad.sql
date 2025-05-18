60574f0c4e039aa440ae55f4381d0d0d9790598f
nicolas097 <nicolasolivares097@gmail.com>
2025-05-18 16:16:02 -0400

    Se ha modificado consulta 04 Balance promedio por banco

--- financialbd/02_Tarjetas sin actividad.sql
+++ financialbd/02_Tarjetas sin actividad.sql
@@ -1,9 +1,12 @@
-SELECT a.account_num AS numero_cuenta,
-       c.card_id AS id_card,
-       round(IFNULL(w.trans_amount, 0), 0) AS monto_retirado,
-		round(IFNULL(d.trans_amount, 0), 0) AS monto_depositado
+SELECT 
+    a.account_num AS numero_cuenta,
+    c.card_id AS id_card,
+    ROUND(IFNULL(w.trans_amount, 0), 0) AS monto_retirado,
+    ROUND(IFNULL(d.trans_amount, 0), 0) AS monto_depositado
 FROM cards c 
 INNER JOIN accounts a ON a.card_id = c.card_id
+LEFT JOIN withdraw w ON w.account_num = a.account_num
+LEFT JOIN deposit d ON d.account_num = a.account_num
 WHERE a.account_num NOT IN (
     SELECT account_num FROM withdraw
 )
